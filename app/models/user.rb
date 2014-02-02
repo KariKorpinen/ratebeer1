c0cf8bc570d67b4f17f04a0aef38eceaad558bbd
Kari Korpinen <karikkorpinen@gmail.com>
2014-02-02 23:53:01 +0200

    viikko3a

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   validates :username, uniqueness: true, length: { minimun: 3, maximum: 15}
   validates :password, length: { minimun: 4}, format: { in: [a-zA-Z0-9]}
-  has_many :ratings #käyttäjällä on monta ratingia
+  has_many :ratings #käyttäjällä on monta ratingia.
   has_many :beers, through: :ratings
 
   has_secure_password
